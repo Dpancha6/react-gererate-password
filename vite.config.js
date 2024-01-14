d73730a8e8b703b69701b1e2299f3e42a0740dc4
Dpancha6 <dpancha6@gmail.com>
2024-01-13 18:42:52 -0700

    updated main.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/password_generator/",
+  base: "/react-gererate-password/",
+  // root: './',
+  // input: '/react-gererate-password/src/main.jsx',
   plugins: [react()],
 });
